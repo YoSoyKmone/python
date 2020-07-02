d336a6d13458d85b09beb6eb805a4182882cb96e
YoSoyKmone <67443286+YoSoyKmone@users.noreply.github.com>
2020-07-02 00:25:39 -0300

    Create for.py

---
+++ for.py
@@ -0,0 +1,4 @@
+numero= int(raw_imput(numero de la tabla de multiplicar)) #esto pide la variable
+for x in range(1,10): #esto da el rango
+    print str(x) + " * " +  str(numero)  + " = " + str((x * numero)) #esta es la condición del loop
+#esto es un ejemplo de un looop con for
