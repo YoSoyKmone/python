ea340cc38dfaf1a893e1cf059bf4d2e7f0226194
YoSoyKmone <67443286+YoSoyKmone@users.noreply.github.com>
2020-07-02 00:26:00 -0300

    Create arreglos.py

---
+++ arreglos.py
@@ -0,0 +1,16 @@
+nota=0
+notas=[]
+i=0
+h=int(raw_imput("cantidad de calificaciones"))
+while nota >= 0:
+    if i!=h
+        nota=int(raw_imput("nota"))
+        notas.append(nota)
+        i=i+1
+    Else
+        print("ya estan todas las calificaciones")
+
+suma=0
+for n in notas
+    suma=suma+n
+print("promedio"+ str(suma/h))
